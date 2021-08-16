6c53d87ee65a2b17addfc6b7f498e21f66eb0f3b
AM <amikh05@gmail.com>
2021-08-16 19:03:56 +0300

    add service

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -3,26 +3,45 @@ package com.controller;
 
 import com.model.Product;
 import com.service.ProductService;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Log
 @RestController
 public class ProductController {
     @Autowired
     final private ProductService productService;
 
     public ProductController(ProductService productService) {
+
         this.productService = productService;
     }
-
-
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/product")
     public List<Product> getProduct() {
         return productService.findAll();
     }
 
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/sla")
+    public String  getSla() {
+        return productService.isSla();
+    }
+
+
+
+
+
+//    @PostMapping(value="/add")
+//    public String addProduct(){
+//       return productService.isAddProduct();
+//
+//    }
 
 }
--- src/main/java/com/service/ProductService.java
+++ src/main/java/com/service/ProductService.java
@@ -1,11 +1,15 @@
 package com.service;
 
 import com.model.Product;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
+@Log
 @Service
 public class ProductService  {
 
@@ -18,6 +22,14 @@ public class ProductService  {
 
 
 
+   public String isSla(){
+       String result=null;
+       int min = 0;
+       int max = 10;
+       int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+       log.info("rundom number : "+random_int);
+       return Integer.toString(random_int);
+   }
 
 
 }
